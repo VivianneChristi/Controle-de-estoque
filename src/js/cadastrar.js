2881441adbf5b65625c329cbfc8698f57d52db95
RaphaCardoso <154370014+RaphaCardoso@users.noreply.github.com>
2024-02-27 14:51:24 -0300

    Update cadastrar.js

--- src/js/cadastrar.js
+++ src/js/cadastrar.js
@@ -1,8 +1,33 @@
-var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+const exampleInputCod = document.getElementById("InputCod");
+
+const exampleInputProd = document.getElementById("InputProd");
+
+const exampleInputFab = document.getElementById("InputFab");
+
+const exampleInputMod = document.getElementById("InputMod");
+
+const exampleInputQnt = document.getElementById("InputQnt");
+
+const exampleInputValue = document.getElementById("InputValue");
+
+const btnInput = document.getElementById("btnInput");
+
+const data = [];
+
+
+btnInput.addEventListener('click', function (event) {
+
+    event.preventDefault();
+
+    const produto = {
+        Nome: exampleInputProd.value,
+        Cod: exampleInputCod.value,
+        Modelo: exampleInputMod.value,
+        Quantidade: exampleInputQnt.value,
+        Valor: exampleInputValue.value,
     }
-}
+
+    data.push(produto);
+    console.log(data);
+});
